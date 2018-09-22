75b6f048ec79eff7d5fbf831ab1a0506953daf83
Shao Eric Hu <erichu11364@gmail.com>
2018-09-21 23:19:21 -0400

    creditcard

--- mobileapp/screens/StoryScreen.js
+++ mobileapp/screens/StoryScreen.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { Button } from 'react-native-elements';
+import { Entypo } from '@expo/vector-icons';
 export default class StoryScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => ({
+    headerTitle: 'Settings',
+    tabBarIcon: () => <Entypo name="news" size={25} color="#03A9F4" />
+  });
   render() {
     return (
       <View>
