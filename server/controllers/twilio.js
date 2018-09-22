68631fba464bc146f1206ce0b17d2c5307b971bb
Quazi <q.hossain@ymail.com>
2018-09-22 10:13:39 -0400

    removed twilio batch

--- server/controllers/twilio.js
+++ server/controllers/twilio.js
@@ -4,22 +4,13 @@ const authToken = process.env.AUTH_TOKEN;
 
 const client = require('twilio')(accountSid, authToken);
 
+/* twilio code send a text to a number provided */
 let twilio = (phone, body) => {
     client.messages
         .create({
             body: body,
             from: '+19082800331',
             to: phone
-        })
-        .then((message) => console.log(message.sid));
-};
-
-let twilioBatch = (phone, body) => {
-    client.messages
-        .create({
-            to: phone,
-            from: '+19082800331',
-            body: `Your classes have been added: \n ${body}`
         })
         .then((message) => console.log(message.sid));
 };
