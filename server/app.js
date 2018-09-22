b71b1a94f7b7921af8dd7975d41a0b426ababbb3
Quazi <q.hossain@ymail.com>
2018-09-22 01:23:58 -0400

    added express-session to keep the session for every user

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const cors = require('cors');
 const morgan = require('morgan');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
@@ -47,6 +48,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('dev'));
+// app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
 
 app.use('/', indexRouter);
 //app.use('/feed', feedRouter);
