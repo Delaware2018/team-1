95b18ef28dae3c0f9609ccca813d2cb2d9a6ab25
Quazi <q.hossain@ymail.com>
2018-09-21 20:59:01 -0400

    fixed minor error

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const { selectStatement } = require('./../db/index');
 
 router.get('/story', (req, res) => {
-    selectStatement(['U.signedUp', 'D.donationAmount', 'D.dateDonated'], 'UsersTable AS U', `WHERE username = '${req.body.username}' INNER JOIN DonationTable AS D on D.userID = U.userID`);
+    selectStatement(['U.signedUp', 'D.donationAmount', 'D.dateDonated'], 'UsersTable AS U', `WHERE username = '${req.body.username}' INNER JOIN DonationTable AS D on D.userID = U.userID`)
         .then(result => {
             res.status(200).send(result);
         }).catch(error => {
