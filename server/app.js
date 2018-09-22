477cc7c1b4ab89d11629ee804f3b88b2b0625eb0
Quazi <q.hossain@ymail.com>
2018-09-21 20:56:08 -0400

    added router for stories

--- server/app.js
+++ server/app.js
@@ -5,12 +5,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const indexRouter = require('./routes/index');
+
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('dev'));
+
+app.use('/', indexRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello World')
---
+++ server/routes/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const { selectStatement } = require('./../db/index');
+
+router.get('/story', (req, res) => {
+    selectStatement(['U.signedUp', 'D.donationAmount', 'D.dateDonated'], 'UsersTable AS U', `WHERE username = '${req.body.username}' INNER JOIN DonationTable AS D on D.userID = U.userID`);
+        .then(result => {
+            res.status(200).send(result);
+        }).catch(error => {
+            console.log(error);
+            res.sendStatus(404);
+        });
+});
+
+module.exports = router;
