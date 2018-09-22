fecb0c76cad37a7f5f3c02a6d867ba49a9484dbf
Syed Javaid <s.java458@gmail.com>
2018-09-22 00:36:42 -0400

    Fixed Post to match database fields and created analytics file

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -16,28 +16,55 @@ const post = props => {
       <FormGroup controlId="formControlsTextarea">
         <ControlLabel>My Post</ControlLabel>
         <Grid>
+            <Row>
+              <Col md={2}>
+                <p>Type</p>
+              </Col>
+              <Col md={10}>
+                <FormGroup controlId="formControlsSelect">
+                  <FormControl componentClass="select" placeholder="select" name='feedType' onChange={props.textHandler}>
+                    <option value="...">...</option>
+                    <option value="Post">Post</option>
+                    <option value="Event">Event</option>
+                    <option value="URL">URL</option>
+                    <option value="Location">Location</option>
+                  </FormControl>
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row>
+            <Col md={2}>
+              <p>Title</p>
+            </Col>
+            <Col md={10}>
+              <FormControl name="feedName" placeholder="Title..." onChange={props.textHandler}/>
+            </Col>
+          </Row>
+            <Row>
+            <Col md={2}>
+              <p>Location</p>
+            </Col>
+            <Col md={10}>
+              <FormControl name="feedLocation" placeholder="Location..." onChange={props.textHandler} />
+            </Col>
+          </Row>
+          <Row>
+            <Col md={2}>
+              <p>URL</p>
+            </Col>
+            <Col md={10}>
+              <FormControl name="feedUrl" placeholder="Enter URL to share..." onChange={props.textHandler} />
+            </Col>
+          </Row>
           <Row>
             <Col md={2}>
               <p>Post</p>
             </Col>
             <Col md={10}>
               <FormControl
-                name="post"
+                name="feedContent"
                 componentClass="textarea"
                 placeholder="Post to be sent to apps"
-                onChange={props.textHandler}
-              />
-            </Col>
-          </Row>
-          <p />
-          <Row>
-            <Col md={2}>
-              <p>URL</p>
-            </Col>
-            <Col md={10}>
-              <FormControl
-                name="url"
-                placeholder="Enter Youtube Link"
                 onChange={props.textHandler}
               />
             </Col>
