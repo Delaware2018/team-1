847013344f36aad70f7f3b7ca2562d9da3daa4f9
Quazi <q.hossain@ymail.com>
2018-09-22 06:50:40 -0400

    added a way for data analytics to gather more information for referral links

--- server/db/index.js
+++ server/db/index.js
@@ -1,3 +1,5 @@
 export { default as selectStatement } from './selectStatement';
 export { default as insertStatement } from './insertStatement';
 export { default as executeProcedure } from './executeProcedure';
+export { default as updateStatement } from './updateStatement';
+
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,8 @@ const router = require('express').Router();
 const { selectStatement, insertStatement } = require('./../db/index');
 
 router.get('/journey/:user', (req, res) => {
-    console.log(req.params.user);
     //'U.memberSince',
-    selectStatement(['D.amount', 'D.dateOfDonation'], 'UsersTable AS U',` INNER JOIN donations AS D on D.userID = U.userID WHERE U.username = '${req.params.user}'`)
+    selectStatement(['D.amount', 'D.dateOfDonation'], 'UsersTable AS U',` INNER JOIN donations AS D on D.userID = U.userID WHERE U.username = '${req.params.user}' ORDER BY D.dateOfDonation`)
         .then(result => {
             result.forEach(val => {
                 val.dateOfDonation = val.dateOfDonation.slice(0, val.dateOfDonation.indexOf('T'));
@@ -31,6 +30,27 @@ router.post('/donation', (req, res) => {
                 };
 
                 insertStatement(donation, 'donations')
+                    .then((result) => {
+                        res.status(200).send(true);
+                    });
+            } else {
+                res.status(200).send(false);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.sendStatus(404);
+        });
+});
+
+router.get('/referral/:user', (req, res) => {
+    selectStatement(['userID', 'UDT.linkUsed'], 'UsersTable AS U', `INNER JOIN UserDataTable AS UDT on UDT.userID = U.userID WHERE username = '${req.params.user}'`)
+        .then(user => {
+            if (user.length === 1) {
+                let data = {
+                    linkedUsed: user[0].linkedUsed++
+                };
+                insertStatement(data, 'UserDataTable')
                     .then((result) => {
                         res.status(200).send(true);
                     });
