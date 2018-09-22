b1bfea82903ba6edd7c060e201051fff8176a88b
Quazi <q.hossain@ymail.com>
2018-09-22 09:50:58 -0400

    created twilio api to assist the app

---
+++ server/controllers/twilio.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+
+const client = require('twilio')(accountSid, authToken);
+
+let twilio = (phone, body) => {
+    client.messages
+        .create({
+            body: body,
+            from: '+19082800331',
+            to: phone
+        })
+        .then((message) => console.log(message.sid));
+};
+
+let twilioBatch = (phone, body) => {
+    client.messages
+        .create({
+            to: phone,
+            from: '+19082800331',
+            body: `Your classes have been added: \n ${body}`
+        })
+        .then((message) => console.log(message.sid));
+};
+
+twilio('7186830434', 'hello');
+
+// module.exports = twilio;
